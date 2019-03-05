25ca0291262aa8d550b52b95a637e2269f377b4d
Thomas D <thomas@astucemedia.com>
2019-03-05 01:24:22 -0500

    change region code

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -4,7 +4,7 @@
 export IMAGE_NAME=ECS
 export IMAGE_VERSION=latest
 
-export AWS_DEFAULT_REGION=ca-central-1b
+export AWS_DEFAULT_REGION=ca-central-1
 export AWS_ECS_CLUSTER_NAME=default
 export AWS_VIRTUAL_HOST=dev.dsnybff.com
 
