924765401d8f5df2b6eb99606c034b5a17681987
Thomas D <thomas@astucemedia.com>
2019-03-08 08:24:09 -0500

    allow location clicks

--- src/dsnybff/wwwroot/assets/js/default.js
+++ src/dsnybff/wwwroot/assets/js/default.js
@@ -1,1 +1,8 @@
-// App's JavaScript
+
+var $carousel = $('.main-locations').flickity();
+
+$carousel.on('staticClick.flickity', function (event, pointer, cellElement, cellIndex) {
+    if (typeof cellIndex == 'number') {
+        $carousel.flickity('selectCell', cellIndex);
+    }
+});
