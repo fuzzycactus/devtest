b9eb8dbf9d5f2d576fede0db8936692557ec3a94
Thomas D <thomas@astucemedia.com>
2019-03-12 08:35:35 -0400

    remove gitignore from types folder

---
+++ src/dsnybff.ServiceModel/Types/Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dsnybff.ServiceModel
+{
+    public class Location
+    {
+        public string Id { get { return Id_; } set { Id_ = value?.Trim(); } }
+        public string Title { get { return Title_; } set { Title_ = value?.Trim(); } }
+        public string Topics { get { return Topics_; } set { Topics_ = value?.Trim(); } }
+        public string Abv { get; set; }
+        public int IconImageId { get; set; }
+
+        private string Id_ = string.Empty;
+        private string Title_ = string.Empty;
+        private string Topics_ = string.Empty;
+    }
+}
---
+++ src/dsnybff.ServiceModel/Types/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dsnybff.ServiceModel
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get { return Title_; } set { Title_ = value?.Trim(); } }
+        public string Topics { get { return Topics_; } set { Topics_ = value?.Trim(); } }
+        public string Locations { get { return Locations_; } set { Locations_ = value?.Trim(); } }
+        public DateTime PublishedDate { get; set; }
+        public string TripDate { get; set; }
+        public string Slides { get; set; }
+        public bool IsDraft { get; set; }
+        public bool HasJr { get; set; }
+        public bool HasSr { get; set; }
+        public bool HasSlides { get; set; }
+
+        private string Title_ = string.Empty;
+        private string Topics_ = string.Empty;
+        private string Locations_ = string.Empty;
+    }
+}
