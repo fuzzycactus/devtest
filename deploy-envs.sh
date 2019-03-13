43f06ef8f23df484a15a45ce68ba6457a809eac5
Thomas D <thomas@astucemedia.com>
2019-03-13 01:29:04 -0400

    testing multiple hosts

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -6,7 +6,7 @@ export IMAGE_VERSION=latest
 
 export AWS_DEFAULT_REGION=ca-central-1
 export AWS_ECS_CLUSTER_NAME=default
-export AWS_VIRTUAL_HOST=dsnybff.com
+export AWS_VIRTUAL_HOST=dsnybff.com,discoveringdisney.com
 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
